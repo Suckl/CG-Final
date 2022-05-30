4f07366eba49bbfc2b03c5a5c7af65bf769c1d28
Suckl <447747035@qq.com>
2022-05-30 15:34:57 +0800

    功能添加完善1
    在前一版本的基础上，实现了通过文件路径加载OBJ与纹理贴图的功能。在对象控制次级页面下能分别调控每个物体的位置、朝向、大小、颜色、纹理、可见性、粗糙度。

--- src/base/scene.cpp
+++ src/base/scene.cpp
@@ -9,23 +9,13 @@ Scene::Scene(const Options& options): Application(options) {
 
     
 	// init model
-    _objectlist.ModelList.push_back(nullptr);
-    _objectlist.ModelList[0].reset(new Model(modelPath));
-    _objectlist.ModelList[0]->scale=glm::vec3(5.0f,5.0f,5.0f);
-
-	_objectlist.ModelList.push_back(nullptr);
-    _objectlist.ModelList[1].reset(new Model(modelPath));
-	_objectlist.ModelList[1]->scale = glm::vec3(3.0f, 3.0f, 3.0f);
-
-    
+    addModel(modelPath,"earth");
+	addModel(modelPath,"planet");
+	_objectlist.ModelList[1]->scale = glm::vec3(5.0f, 5.0f, 5.0f);
 
 	// init materials
-    _objectlist.Texture2DList.push_back(nullptr);
-	_objectlist.Texture2DList[0].reset(new Texture2D(earthTexturePath));
-
-    _objectlist.Texture2DList.push_back(nullptr);
-	_objectlist.Texture2DList[1].reset(new Texture2D(planetTexturePath));
-
+    addTexture(earthTexturePath,"earthTexture");
+    addTexture(planetTexturePath,"planetTexture");
 
 	// init camera
 	_camera.reset(new PerspectiveCamera(
@@ -59,22 +49,26 @@ void Scene::initSimpleShader() {
 		"layout(location = 1) in vec3 aNormal;\n"
 		"layout(location = 2) in vec2 aTexCoord;\n"
 		"out vec2 fTexCoord;\n"
+        "out vec4 fcolor;\n"
 		"uniform mat4 projection;\n"
 		"uniform mat4 view;\n"
 		"uniform mat4 model;\n"
+        "uniform vec3 color\n;"
 
 		"void main() {\n"
 		"	fTexCoord = aTexCoord;\n"
+        "   fcolor = vec4(color,1.0);\n"
 		"	gl_Position = projection * view * model * vec4(aPosition, 1.0f);\n"
 		"}\n";
 
 	const char* fsCode =
 		"#version 330 core\n"
+        "in vec4 fcolor;\n"
 		"in vec2 fTexCoord;\n"
 		"out vec4 color;\n"
 		"uniform sampler2D mapKd;\n"
 		"void main() {\n"
-		"	color = texture(mapKd, fTexCoord);\n"
+		"	color = fcolor*texture(mapKd, fTexCoord);\n"
 		// "	color=vec4(1.0);"
 		"}\n";
 
@@ -96,25 +90,25 @@ void Scene::handleInput() {
 	}
 
 	if (_keyboardInput.keyStates[GLFW_KEY_W] != GLFW_RELEASE&&mouse_capture_flag) {
-		if(_CameraMode==CameraMode::Free)_camera->position += _camera->getFront() * _cameraMoveSpeed * (float)_deltaTime;
+		if(_CameraMode==CameraMode::Free)_camera->position += _camera->getFront() * _cameraMoveSpeed * (float)_deltaTime*2;
         else _camera->position += 
             glm::normalize(glm::vec3(_camera->getFront().x,0.0f,_camera->getFront().z)) * _cameraMoveSpeed * (float)_deltaTime;
 	}
 
 	if (_keyboardInput.keyStates[GLFW_KEY_A] != GLFW_RELEASE&&mouse_capture_flag) {
-		if(_CameraMode==CameraMode::Free)_camera->position -= _camera->getRight() * _cameraMoveSpeed * (float)_deltaTime;
+		if(_CameraMode==CameraMode::Free)_camera->position -= _camera->getRight() * _cameraMoveSpeed * (float)_deltaTime*2;
         else _camera->position -= 
             glm::normalize(glm::vec3(_camera->getRight().x,0.0f,_camera->getRight().z)) * _cameraMoveSpeed * (float)_deltaTime;
 	}
 
 	if (_keyboardInput.keyStates[GLFW_KEY_S] != GLFW_RELEASE&&mouse_capture_flag) {
-		if(_CameraMode==CameraMode::Free)_camera->position -= _camera->getFront() * _cameraMoveSpeed * (float)_deltaTime;
+		if(_CameraMode==CameraMode::Free)_camera->position -= _camera->getFront() * _cameraMoveSpeed * (float)_deltaTime*2;
         else _camera->position -= 
             glm::normalize(glm::vec3(_camera->getFront().x,0.0f,_camera->getFront().z)) * _cameraMoveSpeed * (float)_deltaTime;
 	}
 
 	if (_keyboardInput.keyStates[GLFW_KEY_D] != GLFW_RELEASE&&mouse_capture_flag) {
-		if(_CameraMode==CameraMode::Free)_camera->position += _camera->getRight() * _cameraMoveSpeed * (float)_deltaTime;
+		if(_CameraMode==CameraMode::Free)_camera->position += _camera->getRight() * _cameraMoveSpeed * (float)_deltaTime*2;
         else _camera->position += 
             glm::normalize(glm::vec3(_camera->getRight().x,0.0f,_camera->getRight().z)) * _cameraMoveSpeed * (float)_deltaTime;
 	}
@@ -122,8 +116,8 @@ void Scene::handleInput() {
     if (_keyboardInput.keyStates[GLFW_KEY_LEFT_ALT] != GLFW_RELEASE&&test==false) {
         test=true;
 		mouse_capture_flag=!mouse_capture_flag;
-        if(mouse_capture_flag) {glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);printf("True");}
-        else {glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);printf("flase");}
+        if(mouse_capture_flag) glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        else glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 	}
     if(_keyboardInput.keyStates[GLFW_KEY_LEFT_ALT] == GLFW_RELEASE&&test==true){
         test=false;
@@ -148,7 +142,7 @@ void Scene::handleInput() {
 		// rotate around local right
         if(test) _mouseInput.move.yOld=_mouseInput.move.yCurrent;
 		const float deltaY = static_cast<float>(_mouseInput.move.yCurrent - _mouseInput.move.yOld);
-		const float angle = -cameraRotateSpeed * _deltaTime * deltaY;
+		float angle = -cameraRotateSpeed * _deltaTime * deltaY;
 		const glm::vec3 axis = _camera->getRight();
 		// you should know how quaternion works to represent rotation
 		_camera->rotation = glm::normalize(glm::quat{ cos(angle / 2),axis.x*sin(angle / 2),axis.y*sin(angle / 2),axis.z*sin(angle / 2) }*_camera->rotation);
@@ -170,19 +164,41 @@ void Scene::renderFrame() {
     drawGUI();
 }
 
+bool Scene::addModel(const std::string filename,const std::string name){
+    _objectlist.ModelList.push_back(nullptr);
+    _objectlist.objectname.push_back(name);
+    _objectlist.ModelList[_objectlist.ModelList.size()-1].reset(new Model(filename));
+	_objectlist.ModelList[_objectlist.ModelList.size()-1]->scale = glm::vec3(3.0f, 3.0f, 3.0f);
+    _objectlist.visible.push_back(true);
+    _objectlist.Color.push_back(glm::vec3(1.0));
+    _objectlist.TextureIndex.push_back(0);
+    _objectlist.color_flag.push_back(false);
+    _objectlist.roughness.push_back(0.5f);
+}
+bool Scene::addTexture(const std::string filename,const std::string name){
+    _texturelist.texturename.push_back(name);
+    _texturelist.texture.push_back(nullptr);
+	_texturelist.texture[_texturelist.texture.size()-1].reset(new Texture2D(filename));
+}
+
 // 之后还需要根据ShadowMode分别设置，现在只是简化
 void Scene::drawList(){
     switch(_ShadowRenderMode){
         case ShadowRenderMode::None:
         for(int i=0;i<_objectlist.ModelList.size();i++){
+            if(!_objectlist.visible[i]) continue;
             _simpleShader->use();
             const glm::mat4 projection = _camera->getProjectionMatrix();
             const glm::mat4 view = _camera->getViewMatrix();
             _simpleShader->setMat4("projection", projection);
             _simpleShader->setMat4("view", view);
             _simpleShader->setMat4("model", _objectlist.ModelList[i]->getModelMatrix());
-            glActiveTexture(GL_TEXTURE0);
-            _objectlist.Texture2DList[i]->bind();
+            if(_objectlist.color_flag[i]) _simpleShader->setVec3("color",_objectlist.Color[i]);
+            else _simpleShader->setVec3("color",glm::vec3(1.0f));
+            if(!_objectlist.color_flag[i]&&_texturelist.texture[_objectlist.TextureIndex[i]]!=nullptr){
+                glActiveTexture(GL_TEXTURE0);
+                _texturelist.texture[_objectlist.TextureIndex[i]]->bind();
+            }
             _objectlist.ModelList[i]->draw();
         }
         break;
@@ -255,6 +271,7 @@ void Scene::drawGUI()  {
         ImGui::End();
     }
     if(file_flags){
+        static char loadname[128] = "Type your file/object name here";
         static char filepath[128] = "Type your file path acurrately!";
         ImGui::Begin("File Window", &file_flags);
         ImGui::Text("In this Window, you can load OBJ files or textures with filepath.");
@@ -262,12 +279,25 @@ void Scene::drawGUI()  {
         ImGui::NewLine();
         ImGui::Separator();
         ImGui::InputText("input file path", filepath, IM_ARRAYSIZE(filepath));
+        ImGui::InputText("input file name", loadname, IM_ARRAYSIZE(loadname));
         if(ImGui::Button("Load OBJ file")){
-            // TODO:Load OBJ file
+            std::string filename =filepath;
+            std::string name=loadname;
+            addModel(filename,loadname);
+            filename="Load Success!";
+            name=" ";
+            strcpy(loadname,name.c_str());
+            strcpy(filepath,filename.c_str());
         }
         ImGui::SameLine();
         if(ImGui::Button("Load Texture file")){
-            // TODO:Load Texture file
+            std::string filename =filepath;
+            std::string name=loadname;
+            addTexture(filename,loadname);
+            filename="Load Success!";
+            name=" ";
+            strcpy(loadname,name.c_str());
+            strcpy(filepath,filename.c_str());
         }
         ImGui::Separator();
         ImGui::Text("This will merge all the objects to a single group.");
@@ -279,10 +309,84 @@ void Scene::drawGUI()  {
     }
     if(object_flags){
         ImGui::Begin("Object Window", &object_flags);
-        ImGui::Text("In this Window, you can view all the objects in the scene including light.");
-        ImGui::Text("You are free to set their scales, rotations and locations.");
-        ImGui::Text("For normal object, you can set roughness and color.");
-        ImGui::Text("For light, you can set intensity and color");
+        ImGui::Text("In this Window, you can view all the objects in the ");
+        ImGui::Text("scene including light.You are free to set their scales, ");
+        ImGui::Text("rotations and locations.For normal object, you can set ");
+        ImGui::Text("roughness and color.For light, you can set intensity and color.");
+        if(ImGui::CollapsingHeader("Object List")){
+            for (int i =0;i<_objectlist.ModelList.size();i++){
+                if(ImGui::TreeNode((void*)(intptr_t)i,"object %s",_objectlist.objectname[i].c_str())){
+                    ImVec4 color = ImVec4(_objectlist.Color[i].x, _objectlist.Color[i].y, _objectlist.Color[i].z,1.0f);
+                    const float rota_angle=0.05f;
+                    float position[3]={_objectlist.ModelList[i]->position.x,_objectlist.ModelList[i]->position.y,_objectlist.ModelList[i]->position.z};
+                    float scale[3] = {_objectlist.ModelList[i]->scale.x,_objectlist.ModelList[i]->scale.y,_objectlist.ModelList[i]->scale.z};
+                    bool color_flag=_objectlist.color_flag[i];
+                    float roughness=_objectlist.roughness[i];
+                    ImGui::Checkbox("Use color instead of texture",&color_flag);
+                    _objectlist.color_flag[i]=color_flag;
+                    ImGui::ColorEdit3("Object Color",(float*)&color);
+                    _objectlist.Color[i]=glm::vec3(color.x,color.y,color.z);
+                    ImGui::SliderFloat("Roughness",&roughness,0.0f,1.0f,"%.2f");
+                    _objectlist.roughness[i]=roughness;
+                    ImGui::DragFloat3("Scale",scale,0.005f,0.0f,10.0f,"%.3f");ImGui::SameLine();
+                    if(ImGui::Button("Proportional base scale.x")){
+                        scale[2]=scale[1]=scale[0];  
+                    }
+                    ImGui::DragFloat3("Position",position,0.005f,-100.0f,100.0f,"%.3f");
+                    _objectlist.ModelList[i]->scale=glm::vec3{scale[0],scale[1],scale[2]};
+                    _objectlist.ModelList[i]->position=glm::vec3{position[0],position[1],position[2]};
+                    float spacing = ImGui::GetStyle().ItemInnerSpacing.x;
+                    ImGui::PushButtonRepeat(true);
+                    ImGui::Text("Hode to rotate:   ");ImGui::SameLine();
+                    ImGui::Text("rotation-x");ImGui::SameLine();
+                    if (ImGui::ArrowButton("##left", ImGuiDir_Left)) {
+                        _objectlist.ModelList[i]->rotation =glm::normalize(glm::quat{ cos(rota_angle / 2),0.0,1.0*sin(rota_angle / 2),0.0 }*_objectlist.ModelList[i]->rotation);
+                    }
+                    ImGui::SameLine(0.0f, spacing);
+                    if (ImGui::ArrowButton("##right", ImGuiDir_Right)) {
+                        _objectlist.ModelList[i]->rotation =glm::normalize(glm::quat{ cos(rota_angle / 2),0.0,-1.0*sin(rota_angle / 2),0.0 }*_objectlist.ModelList[i]->rotation);
+                    }
+                    ImGui::SameLine();ImGui::Text("rotation-y");ImGui::SameLine();
+                    if (ImGui::ArrowButton("##left", ImGuiDir_Left)) {
+                        _objectlist.ModelList[i]->rotation =glm::normalize(glm::quat{ cos(rota_angle / 2),1.0*sin(rota_angle / 2),0.0,0.0 }*_objectlist.ModelList[i]->rotation);
+                    }
+                    ImGui::SameLine(0.0f, spacing);
+                    if (ImGui::ArrowButton("##right", ImGuiDir_Right)) {
+                        _objectlist.ModelList[i]->rotation =glm::normalize(glm::quat{ cos(rota_angle / 2),-1.0*sin(rota_angle / 2),0.0,0.0 }*_objectlist.ModelList[i]->rotation);
+                    }
+                    ImGui::SameLine();ImGui::Text("rotation-z");ImGui::SameLine();
+                    if (ImGui::ArrowButton("##left", ImGuiDir_Left)) {
+                        _objectlist.ModelList[i]->rotation =glm::normalize(glm::quat{ cos(rota_angle / 2),0.0,0.0,1.0*sin(rota_angle / 2) }*_objectlist.ModelList[i]->rotation);
+                    }
+                    ImGui::SameLine(0.0f, spacing);
+                    if (ImGui::ArrowButton("##right", ImGuiDir_Right)) {
+                        _objectlist.ModelList[i]->rotation =glm::normalize(glm::quat{ cos(rota_angle / 2),0.0,0.0,-1.0*sin(rota_angle / 2) }*_objectlist.ModelList[i]->rotation);
+                    }
+                    ImGui::PopButtonRepeat();
+                    if(!_objectlist.visible[i]&&ImGui::Button("Click me to display this object")) _objectlist.visible[i]=true;
+                    else if(_objectlist.visible[i]&&ImGui::Button("Click me to hide this object")) _objectlist.visible[i]=false;
+                    int texture_current_idx = _objectlist.TextureIndex[i]; // Here we store our selection data as an index.
+                    ImGui::Text("Click to choose texture for object");
+                    if (ImGui::BeginListBox("TextureList"))
+                    {
+                        for (int n = 0; n < _texturelist.texturename.size(); n++)
+                        {
+                            const bool is_selected = (texture_current_idx == n);
+                            if (ImGui::Selectable(_texturelist.texturename[n].c_str(), is_selected)){
+                                texture_current_idx = n;
+                                _objectlist.TextureIndex[i]=texture_current_idx;
+                            }
+                            if (is_selected)
+                                ImGui::SetItemDefaultFocus();
+                        }
+                        ImGui::EndListBox();
+                    }
+                    ImGui::TreePop();
+                }
+            }
+        }
+
+
         ImGui::End();
     }
     if(NURBS_flags){
--- src/base/scene.h
+++ src/base/scene.h
@@ -21,10 +21,17 @@ enum class CameraMode {
 };
 
 struct ObjectList{
-    std::vector<std::unique_ptr<Model>> ModelList;
-    std::vector<std::shared_ptr<Texture2D>> Texture2DList;
-    // 针对每个物体单独设置Shader，现在看来意义不大
-    // std::vector<std::shared_ptr<GLSLProgram>> ShaderList;
+    std::vector<std::string> objectname;//物体名称
+    std::vector<bool> visible;//可见性
+    std::vector<std::unique_ptr<Model>> ModelList;//物体模型
+    std::vector<bool> color_flag;//应用纹理还是颜色
+    std::vector<int> TextureIndex;//纹理坐标
+    std::vector<glm::vec3> Color;//颜色坐标
+    std::vector<float> roughness;//粗糙度
+};
+struct TextureList{
+    std::vector<std::shared_ptr<Texture2D>> texture;
+    std::vector<std::string> texturename;
 };
 
 class Scene:public Application{
@@ -34,6 +41,7 @@ public:
 private:
     // 物体清单，分别录入了模型纹理以及对应的Shader
     ObjectList _objectlist;
+    TextureList _texturelist;
     std::vector<std::unique_ptr<Light>> LightList;
     // 储存了所有类型的Shader，如果之后创建了许多Shader的话可以用enum来从里面取，现在还没有
     // std::vector<std::shared_ptr<GLSLProgram>> AllShader;
@@ -50,8 +58,9 @@ private:
 
     void initSimpleShader();
     void drawList();
-    // void addModel() const;
-    // void export() const;
+    bool addTexture(const std::string filename,const std::string name);
+    bool addModel(const std::string filename,const std::string name);
+    // void export();
     void drawGUI() ;
     void handleInput() override;
 	void renderFrame() override;
