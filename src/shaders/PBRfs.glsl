897004e09827a1cd8ef76e0289070c09ed65df88
Maid233 <74146511+Maid233@users.noreply.github.com>
2022-06-03 21:14:16 +0800

    修改了阴影位置

--- src/shaders/PBRfs.glsl
+++ src/shaders/PBRfs.glsl
@@ -2,6 +2,7 @@
 uniform vec3 uCameraPos;
 uniform vec3 uLightRadiance;
 uniform vec3 uLightPos;
+uniform float ka;
 
 uniform sampler2D uAlbedoMap;
 uniform float uMetallic;
@@ -41,35 +42,37 @@ vec3 fresnelSchlick(vec3 F0, vec3 V, vec3 H)
 }
 
 void main(void) {
-  vec3 uLightDir = normalize(uLightPos);
-  vec3 albedo = pow(texture2D(uAlbedoMap, vTextureCoord).rgb, vec3(2.2));
-  if(albedo==vec3(0.0)) albedo=uColor;
-  vec3 N = normalize(vNormal);
-  vec3 V = normalize(uCameraPos - vFragPos);
-  float NdotV = max(dot(N, V), 0.0);
-  vec3 F0 = vec3(0.04); 
-  F0 = mix(F0, albedo,uMetallic);
+    vec3 ambient = ka * vec3(0.3);
+    vec3 uLightDir = normalize(uLightPos - vFragPos);
+    vec3 albedo = pow(texture2D(uAlbedoMap, vTextureCoord).rgb, vec3(2.2));
+    if(albedo == vec3(0.0)) albedo = uColor;
 
-  vec3 Lo = vec3(0.0);
+    vec3 N = normalize(vNormal);
+    vec3 V = normalize(uCameraPos - vFragPos);
+    float NdotV = max(dot(N, V), 0.0);
+    vec3 F0 = vec3(0.04); 
+    F0 = mix(F0, albedo,uMetallic);
 
-  vec3 L = normalize(uLightDir);
-  vec3 H = normalize(V + L);
-  float NdotL = max(dot(N, L), 0.0); 
+    vec3 Lo = vec3(0.0);
 
-  vec3 radiance = uLightRadiance;
+    vec3 L = normalize(uLightDir);
+    vec3 H = normalize(V + L);
+    float NdotL = max(dot(N, L), 0.0); 
 
-  float NDF = DistributionGGX(N, H, uRoughness);   
-  float G   = GeometrySmith(N, V, L, uRoughness); 
-  vec3 F = fresnelSchlick(F0, V, H);
+    vec3 radiance = uLightRadiance;
 
-  vec3 numerator    = NDF * G * F; 
-  float denominator = max((4.0 * NdotL * NdotV), 0.001);
-  vec3 BRDF = numerator / denominator;
+    float NDF = DistributionGGX(N, H, uRoughness);   
+    float G   = GeometrySmith(N, V, L, uRoughness); 
+    vec3 F = fresnelSchlick(F0, V, H);
 
-  Lo += BRDF * radiance * NdotL;
-  vec3 color = Lo+vec3(0.001)*texture2D(uAlbedoMap, vTextureCoord).rgb;
+    vec3 numerator    = NDF * G * F; 
+    float denominator = max((4.0 * NdotL * NdotV), 0.001);
+    vec3 BRDF = numerator / denominator;
 
-  color = color / (color + vec3(1.0));
-  color = pow(color, vec3(1.0/2.2)); 
-  gl_FragColor = vec4(color, 1.0);
+    Lo += BRDF * radiance * NdotL;
+    vec3 color = Lo + vec3(0.001) * texture2D(uAlbedoMap, vTextureCoord).rgb;
+
+    color = color / (color + vec3(1.0));
+    color = pow(color, vec3(1.0/2.2)) + ambient; 
+    gl_FragColor = vec4(color, 1.0);
 }
